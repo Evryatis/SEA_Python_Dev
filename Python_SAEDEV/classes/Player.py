cfa1b1d4f3f664123810f55229bdfe9332e067ac
Dani RF <rfernandesdani@gmail.com>
2023-12-01 14:26:46 +0100

    Updated Player.py and fixed the methods

--- Python_SAEDEV/classes/Player.py
+++ Python_SAEDEV/classes/Player.py
@@ -1,21 +1,30 @@
 class Player:
+    max_energy = 10
+
     def __init__(self):
         self.coordinates = (10, 10)
         self.coding_level = 1
         self.energy_maximum = 1
         self.energy = 1
-        self.bitcoin = 0
+        self.bitcoins = 0
 
-    def upgrade_coding(self, bitcoin, coding_level):
-        if bitcoin >= 10:
+    def move_player(self, x_movement, y_movement):
+        self.coordinates = (self.coordinates[0] + x_movement,
+                            self.coordinates[1] + y_movement)
+
+    def upgrade_coding(self):
+        if self.bitcoins >= 10:
             self.coding_level += 1
-            bitcoin -= 10
+            self.bitcoins -= 10
 
-    def upgrade_energy(self, bitcoin, energy_maximum):
-        if bitcoin >= 10:
+    def upgrade_energy(self):
+        if self.bitcoins >= 10:
             self.energy_maximum += 1
-            bitcoin -= 10
+            self.bitcoins -= 10
 
-    def repos(self, energy, energy_maximum):
-        while energy <= energy_maximum:
-            energy += 0.1
+    def add_bitcoins(self, amount):
+        self.bitcoins += amount
+
+    def rest(self):
+        while self.energy <= self.max_energy:
+            self.energy += 1
