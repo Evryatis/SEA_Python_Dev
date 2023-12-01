3837ceac4441ad04b7ad9f23581c3f93d976575e
Dani RF <rfernandesdani@gmail.com>
2023-12-01 08:49:24 +0100

    Tried to make it so it works bruh

--- Python_SAEDEV/classes/Mission.py
+++ Python_SAEDEV/classes/Mission.py
@@ -2,7 +2,7 @@ import random as rd
 
 class Mission:
     mission_list = []
-    mission_coordinates = {}
+    mission_coordinates = set()
 
     def __init__(self):
         self.coordinates = (rd.randint(0, 20), rd.randint(0, 20))
@@ -11,16 +11,28 @@ class Mission:
         self.workload = self.difficulty
         self.id = -1 # Initiate Mission ID which represents its index in the mission_list class variable
 
-        # Change every mission ID each time a new mission is created so IDs still work as indices
-        for i in range(len(Mission.mission_list)):
-            Mission.mission_list.id = i
-    def __del__(self): # Called upon using "del mission"
-        Mission.mission_list.pop(self.id)
+        # Add mission to the mission_list for easier access later on
+        Mission.mission_list.append(self)
 
-        # Change every mission ID each time a mission is destructed so IDs still work as indices
-        for i in range(len(Mission.mission_list)):
-            Mission.mission_list.id = i
+        self.update_mission_list()
+    def __del__(self): # Called upon using "del mission"
+        # print(self.mission_list, self.mission_coordinates)
+        self.mission_list.pop(self.id)
+        self.mission_coordinates.remove(self.coordinates)
+        # print(self.mission_list, self.mission_coordinates)
+
+        self.update_mission_list()
 
     @classmethod
     def get_mission_list(cls):
-        return Mission.mission_list
+        return cls.mission_list
+
+    @classmethod
+    def get_all_mission_coordinates(cls):
+        return cls.mission_coordinates
+
+    @classmethod
+    def update_mission_list(cls):
+        # Change every mission ID each time a new mission is created so IDs still work as indices
+        for i in range(len(cls.mission_list)):
+            cls.mission_list[i].id = i
