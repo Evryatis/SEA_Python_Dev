5b88537b2efeba4d4c57879510b4b226d325084d
Dani RF <rfernandesdani@gmail.com>
2023-12-01 09:51:57 +0100

    Accomodated main.py for testing purposes

--- Python_SAEDEV/main.py
+++ Python_SAEDEV/main.py
@@ -2,7 +2,6 @@ from functions import ReadAndWrite as rw
 from classes.Player import Player
 from classes.Mission import Mission
 
-
 # Creating game map
 
 game_map = [["-1" for j in range(21)] for i in range(21)]
@@ -10,6 +9,8 @@ game_center = 0
 game_map[10][10] = game_center
 
 # rw.write_map_file(game_map, "./data")
+
+map_coordinates = rw.read_map_file("./data")
 
 
 # Game initialisation
