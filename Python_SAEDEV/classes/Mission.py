fd3db3b64491a0dd672b66cd9995ca86ff71d0f5
Dani RF <rfernandesdani@gmail.com>
2023-12-01 09:21:05 +0100

    it still no workey :(((((((

--- Python_SAEDEV/classes/Mission.py
+++ Python_SAEDEV/classes/Mission.py
@@ -11,19 +11,23 @@ class Mission:
         self.workload = self.difficulty
 
         # Add mission to the mission_list for easier access later on
-        Mission.add_mission_to_class_var(self)
+        Mission.mission_list.append(self)
+        Mission.mission_coordinates.add(self.coordinates)
 
         # Initiate Mission ID which represents its index in the mission_list class variable
         self.id = len(Mission.mission_list) - 1
 
-        self.update_mission_list()
-
+        # Change every mission ID each time a new mission is created so IDs still work as indices
+        for i in range(len(Mission.mission_list)):
+            Mission.mission_list[i].id = i
     def __del__(self): # Called upon using "del mission"
         # print(self.mission_list, self.mission_coordinates)
-        Mission.remove_mission_from_class_var(self)
+        Mission.mission_list.pop(self.id)
+        Mission.mission_coordinates.remove(self.coordinates)
         # print(self.mission_list, self.mission_coordinates)
 
-        self.update_mission_list()
+        for i in range(len(Mission.mission_list)):
+            Mission.mission_list[i].id = i
 
     @classmethod
     def get_mission_list(cls):
@@ -32,19 +36,3 @@ class Mission:
     @classmethod
     def get_all_mission_coordinates(cls):
         return cls.mission_coordinates
-
-    @classmethod
-    def update_mission_list(cls):
-        # Change every mission ID each time a new mission is created so IDs still work as indices
-        for i in range(len(cls.mission_list)):
-            cls.mission_list[i].id = i
-
-    @classmethod
-    def add_mission_to_class_var(cls, object):
-        cls.mission_list.append(object)
-        cls.mission_coordinates.add(object.coordinates)
-
-    @classmethod
-    def remove_mission_from_class_var(cls, object):
-        cls.mission_list.pop(object.id)
-        cls.mission_coordinates.remove(object.coordinates)
