e07e3adcb2bbe71b6f9e4f034605782b000a053c
Dani RF <rfernandesdani@gmail.com>
2023-12-01 09:25:33 +0100

    IT WORKS MASHALLAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Python_SAEDEV/classes/Mission.py
+++ Python_SAEDEV/classes/Mission.py
@@ -1,7 +1,6 @@
 import random as rd
 
 class Mission:
-    mission_list = []
     mission_coordinates = set()
 
     def __init__(self):
@@ -10,28 +9,11 @@ class Mission:
         self.difficulty = rd.randint(1, 9)
         self.workload = self.difficulty
 
-        # Add mission to the mission_list for easier access later on
-        Mission.mission_list.append(self)
+        # Add mission coordinates to the mission_coordinates set for easier access later on
         Mission.mission_coordinates.add(self.coordinates)
 
-        # Initiate Mission ID which represents its index in the mission_list class variable
-        self.id = len(Mission.mission_list) - 1
-
-        # Change every mission ID each time a new mission is created so IDs still work as indices
-        for i in range(len(Mission.mission_list)):
-            Mission.mission_list[i].id = i
     def __del__(self): # Called upon using "del mission"
-        # print(self.mission_list, self.mission_coordinates)
-        Mission.mission_list.pop(self.id)
         Mission.mission_coordinates.remove(self.coordinates)
-        # print(self.mission_list, self.mission_coordinates)
-
-        for i in range(len(Mission.mission_list)):
-            Mission.mission_list[i].id = i
-
-    @classmethod
-    def get_mission_list(cls):
-        return cls.mission_list
 
     @classmethod
     def get_all_mission_coordinates(cls):
