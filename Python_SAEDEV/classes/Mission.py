92c0261572e4cc4a5e3810f9549528e273f3effc
Dani RF <rfernandesdani@gmail.com>
2023-12-01 08:25:58 +0100

    Fixed the Mission class and added a class "mission_coordinates" set

--- Python_SAEDEV/classes/Mission.py
+++ Python_SAEDEV/classes/Mission.py
@@ -2,9 +2,11 @@ import random as rd
 
 class Mission:
     mission_list = []
+    mission_coordinates = {}
 
     def __init__(self):
         self.coordinates = (rd.randint(0, 20), rd.randint(0, 20))
+        Mission.mission_coordinates.add(self.coordinates)
         self.difficulty = rd.randint(1, 9)
         self.workload = self.difficulty
         self.id = -1 # Initiate Mission ID which represents its index in the mission_list class variable
@@ -13,7 +15,7 @@ class Mission:
         for i in range(len(Mission.mission_list)):
             Mission.mission_list.id = i
     def __del__(self): # Called upon using "del mission"
-        Mission.mission_amount.pop(self.id)
+        Mission.mission_list.pop(self.id)
 
         # Change every mission ID each time a mission is destructed so IDs still work as indices
         for i in range(len(Mission.mission_list)):
