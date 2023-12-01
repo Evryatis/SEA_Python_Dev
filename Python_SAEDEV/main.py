37714104f3d8dbbd9c69b98cbbbf29b1b0b8651d
Dani RF <rfernandesdani@gmail.com>
2023-12-01 08:49:35 +0100

    Updated main.py for testing

--- Python_SAEDEV/main.py
+++ Python_SAEDEV/main.py
@@ -1,6 +1,6 @@
 from functions import ReadAndWrite as rw
-from classes import Player
-from classes import Mission
+from classes.Player import Player
+from classes.Mission import Mission
 
 
 # Creating game map
@@ -9,20 +9,21 @@ game_map = [["-1" for j in range(21)] for i in range(21)]
 game_center = 0
 game_map[10][10] = game_center
 
-rw.write_map_file(game_map, "./data")
+# rw.write_map_file(game_map, "./data")
 
 
 # Game initialisation
 
 player1 = Player()
 
+print(Mission.get_mission_list(), Mission.get_all_mission_coordinates())
 mission1 = Mission()
-print(mission1)
+print(Mission.get_mission_list(), Mission.get_all_mission_coordinates())
+del mission1
+print(Mission.get_mission_list(), Mission.get_all_mission_coordinates())
 
 
 # Game loop
 
 def game_loop():
     pass
-
-
