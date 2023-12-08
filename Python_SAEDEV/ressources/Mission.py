9600306bb3b38022f90d52c3360719a10f99fef7
Dani RF <rfernandesdani@gmail.com>
2023-12-08 14:40:00 +0100

    Fixed an issue where creating two Mission objects with the same coordinates made an error

--- Python_SAEDEV/ressources/Mission.py
+++ Python_SAEDEV/ressources/Mission.py
@@ -6,15 +6,16 @@ class Mission:
 
     def __init__(self, coordinates, difficulty):
         self.coordinates = coordinates
-        Mission.mission_coordinates.add(self.coordinates)
+        if self.coordinates not in Mission.mission_coordinates:
+            Mission.mission_coordinates.add(self.coordinates)
         self.difficulty = difficulty
         self.workload = self.difficulty
 
         # Add mission coordinates to the mission_coordinates set for easier access later on
         Mission.mission_coordinates.add(self.coordinates)
 
-    def __del__(self):  # Called upon using "del mission"
-        Mission.mission_coordinates.remove(self.coordinates)
+    def remove_from_mission_coordinates(self):
+        self.mission_coordinates.remove(self.coordinates)
 
     @classmethod
     def get_all_mission_coordinates(cls):
