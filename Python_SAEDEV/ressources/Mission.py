201ee375d9bc29132e756720bdb6787b40ac9fa2
Dani RF <rfernandesdani@gmail.com>
2023-12-12 15:22:50 +0100

    it work now lmao

--- Python_SAEDEV/ressources/Mission.py
+++ Python_SAEDEV/ressources/Mission.py
@@ -4,18 +4,23 @@ import random as rd
 class Mission:
     mission_coordinates = set()
 
-    def __init__(self, coordinates, difficulty):
+    def __init__(self, coordinates, difficulty, workload):
         self.coordinates = coordinates
         if self.coordinates not in Mission.mission_coordinates:
             Mission.mission_coordinates.add(self.coordinates)
         self.difficulty = difficulty
-        self.workload = self.difficulty
+        self.starting_workload = workload
+        self.remaining_workload = workload
 
         # Add mission coordinates to the mission_coordinates set for easier access later on
         Mission.mission_coordinates.add(self.coordinates)
 
-    def remove_from_mission_coordinates(self):
-        self.mission_coordinates.remove(self.coordinates)
+    def __del__(self):
+        Mission.mission_coordinates.clear()
+
+    def get_reward(self):
+        if self.remaining_workload == 0:
+            return
 
     @classmethod
     def get_all_mission_coordinates(cls):
